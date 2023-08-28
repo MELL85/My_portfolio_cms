8a32c66745b91eb378a214ca22306d94920d1766
Yevhen <melnyk_evgenij@icloud.com>
2023-08-28 12:30:47 +0300

    created all template for sanity and backend is done

--- backend_sanity/schemas/index.ts
+++ backend_sanity/schemas/index.ts
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import abouts from './abouts';
+import contact from './contact';
+import experiences from './experiences';
+import services from './services';
+import skills from './skills';
+import testimonials from './testimonials';
+import workExperiences from './workExperiences';
+import works from './works';
+
+export const schemaTypes = [
+    abouts,
+    contact,
+    experiences,
+    services,
+    skills,
+    testimonials,
+    workExperiences,
+    works
+]
